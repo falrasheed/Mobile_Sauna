e9391084cc06380701340029a87cc700468386fb
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-14 12:17:00 +0200

    Adding photo to the user

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_094510) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "user_photo"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
