435882d72665c9146f6b75b253ef946c4b66873f
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-14 11:26:41 +0200

    Adding title to new sauna form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_131548) do
+ActiveRecord::Schema.define(version: 2019_08_14_073008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_08_13_131548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "title"
+    t.string "city"
     t.index ["user_id"], name: "index_saunas_on_user_id"
   end
 
@@ -50,7 +52,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_131548) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "user_photo"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
