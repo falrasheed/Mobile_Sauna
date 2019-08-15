daabda2cb64d597eeb3e46150b480ec81946bb04
Ferdinand Croy <af.croy@yahoo.de>
2019-08-15 17:12:16 +0200

    commit

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,31 +1,5 @@
 Geocoder.configure(
-<<<<<<< HEAD
 
-  units: :km,
-=======
->>>>>>> 74a7a0ebe914252b81ce8de3eb28c9f05293cca5
-  # Geocoding options
-  # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
-  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
-  # use_https: false,           # use HTTPS for lookup requests? (if supported)
-  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-
-  # Exceptions that should not be rescued by default
-  # (if you want to implement custom error handling);
-  # supports SocketError and Timeout::Error
-  # always_raise: [],
-
-  # Calculation options
-<<<<<<< HEAD
-  # units: :mi,                 # :km for kilometers or :mi for miles
-=======
-    units: :km,                 # :km for kilometers or :mi for miles
->>>>>>> 74a7a0ebe914252b81ce8de3eb28c9f05293cca5
-  # distances: :linear          # :spherical or :linear
+  units: :km
+# distances: :linear          # :spherical or :linear
 )
