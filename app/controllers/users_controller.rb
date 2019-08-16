66994a663e6dffd8bd339edf317ef50432be0544
Zach Alvstad <learnwithzach@gmail.com>
2019-08-16 13:19:02 +0200

    Created the profile page, you can edit it too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,5 @@
-# class UsersController < ApplicationController
-#   def new
-#     @user = User.new
-#   end
-
-#   def create
-#     @user = User.new(params[user_strong_params])
-#     @user.save
-#   end
-
-#   def user_strong_params
-#   params.require(:user).permit(:first_name, :last_name, :email, :password)
-#   end
-# end
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show, :delete]
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
